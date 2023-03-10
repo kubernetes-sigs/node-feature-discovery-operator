89939af01502b35e3985035056500ed56d8bf384
Markus Lehtonen <markus.lehtonen@intel.com>
2023-03-10 12:07:29 +0200

    scripts/test-infra: bump golangci-lint to v1.51.2

--- scripts/test-infra/verify.sh
+++ scripts/test-infra/verify.sh
@@ -2,7 +2,7 @@
 
 # Install deps
 gobinpath="$(go env GOPATH)/bin"
-curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- -b "$gobinpath" v1.45.2
+curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- -b "$gobinpath" v1.51.2
 export PATH=$PATH:$gobinpath
 
 # Run verify steps
